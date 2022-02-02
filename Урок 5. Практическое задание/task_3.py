3d464317f748e3e4a222fb9fc4bf44038c0b761e
kadr <kadr_job@mail.ru>
2022-02-02 14:22:50 +0300

    Lesson5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -25,3 +25,178 @@ appendleft, popleft, extendleft дека и соответствующих им 
 для того, чтобы снизить погрешность, желательно операции по каждой ф-ции
 (append, pop и т.д.) проводить в циклах
 """
+from collections import deque
+from timeit import timeit as ti
+
+
+# операции со очередью
+class DeqTest:
+    def __init__(self, test_list):
+        self.obj = deque(test_list)
+
+    def append(self, val):
+        return self.obj.append(val)
+
+    def pop(self):
+        return self.obj.pop()
+
+    def extend(self, val):
+        return self.obj.extend(val)
+
+    def appendleft(self, val):
+        return self.obj.appendleft(val)
+
+    def popleft(self):
+        return self.obj.popleft()
+
+    def extendleft(self, val):
+        self.obj.extendleft(val)
+
+    def get(self, idx):
+        return self.obj[idx]
+
+    def get_by_index(self, idx):
+        return self.obj.index(idx)
+
+
+# операции со списком
+class ListTest:
+    def __init__(self, test_list):
+        self.obj = test_list
+
+    def append(self, val):
+        return self.obj.append(val)
+
+    def pop(self):
+        return self.obj.pop()
+
+    def extend(self, val):
+        return self.obj.extend(val)
+
+    def appendleft(self, val):
+        return self.obj.insert(0, val)
+
+    def popleft(self):
+        res = self.obj[0]
+        self.obj = self.obj[1::]
+        return res
+
+    def extendleft(self, val):
+        for v in val:
+            self.obj.insert(0, v)
+        return self.obj
+
+    def get(self, idx):
+        return self.obj[idx]
+
+    def get_by_index(self, idx):
+        return self.obj.index(idx)
+
+
+def test_list1():
+    print('#############################')
+    print('Task1')
+    print('Test LIST')
+    print('Append time {}'.format(
+        ti('obj.append("z")', setup='obj = ListTest(s_list)', globals=globals(), number=100000)))
+    print('Pop time {}'.format(ti('obj.pop()', setup='obj = ListTest(s_list)', globals=globals(), number=100000)))
+    print('Extend time {}'.format(
+        ti('obj.extend("k")', setup='obj = ListTest(s_list)', globals=globals(), number=100000)))
+
+
+def test_list2():
+    print('#############################')
+    print('Task2')
+    print('Test LIST')
+    print('Appendleft time {}'.format(
+        ti('obj.appendleft("z")', setup='obj = ListTest(s_list)', globals=globals(), number=1000)))
+    print('Popleft time {}'.format(ti('obj.popleft()', setup='obj = ListTest(s_list)', globals=globals(), number=1000)))
+    print('Extendleft time {}'.format(
+        ti('obj.extendleft("kmr")', setup='obj = ListTest(s_list)', globals=globals(), number=1000)))
+
+
+def test_list3():
+    print('#############################')
+    print('Task3')
+    print('Test LIST')
+    print(
+        'Get by index time {}'.format(ti('obj.get(4)', setup='obj = ListTest(s_list)', globals=globals(), number=1000)))
+    print('Get elem by index time {}'.format(
+        ti('obj.get_by_index("z")', setup='obj = ListTest(s_list)', globals=globals(), number=1000)))
+
+
+def test_deque1():
+    print('#############################')
+    print('Task1')
+    print('Test DEQUE')
+    print(
+        'Append time {}'.format(ti('obj.append("z")', setup='obj = DeqTest(s_list)', globals=globals(), number=100000)))
+    print('Pop time {}'.format(ti('obj.pop()', setup='obj = DeqTest(s_list)', globals=globals(), number=100000)))
+    print(
+        'Extend time {}'.format(ti('obj.extend("k")', setup='obj = DeqTest(s_list)', globals=globals(), number=100000)))
+
+
+def test_deque2():
+    print('#############################')
+    print('Task2')
+    print('Test DEQUE')
+    print('Appendleft time {}'.format(
+        ti('obj.appendleft("z")', setup='obj = DeqTest(s_list)', globals=globals(), number=1000)))
+    print('Popleft time {}'.format(ti('obj.popleft()', setup='obj = DeqTest(s_list)', globals=globals(), number=1000)))
+    print('Extendleft time {}'.format(
+        ti('obj.extendleft("kmr")', setup='obj = DeqTest(s_list)', globals=globals(), number=1000)))
+
+
+def test_deque3():
+    print('#############################')
+    print('Task3')
+    print('Test DEQUE')
+    print(
+        'Get by index time {}'.format(ti('obj.get(4)', setup='obj = DeqTest(s_list)', globals=globals(), number=1000)))
+    print('Get elem by index time {}'.format(
+        ti('obj.get_by_index("z")', setup='obj = DeqTest(s_list)', globals=globals(), number=1000)))
+
+
+s_list = list("qazwsxtgtydsfsdsa")
+test_list1()
+test_deque1()
+# print("""Результат
+# Task1
+# Test LIST
+# Append time 0.020471711999999996
+# Pop time 0.018747126999999995
+# Extend time 0.025350098
+# #############################
+# Task1
+# Test DEQUE
+# Append time 0.019220955999999997
+# Pop time 0.016107981000000007
+# Extend time 0.023748609000000004
+# """)
+# print("""Вывод.
+# 'Правые' операции со списком и очередью производятся примерно с одинаковой скоростью, тк нет переупорядочивания элементов списка
+# """)
+
+test_list2()
+test_deque2()
+# print("""Результат
+# Task2
+# Test LIST
+# Appendleft time 0.06078079
+# Popleft time 0.484284813
+# Extendleft time 0.18462985499999995
+# #############################
+# Task2
+# Test DEQUE
+# Appendleft time 0.00020072700000006716
+# Popleft time 0.00015804500000005106
+# Extendleft time 0.0002777400000000263
+#############################
+# """)
+# print("""Вывод.
+# 'Левые' операции гораздо быстрее производятся в очереди, чем в списке т.к. в списке происходит
+# постоянное переупорядочивание элементов, а очередь имеет свои оптимизированные алгоритмы для таких операций
+# """)
+
+test_list3()
+test_deque3()
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,94 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+
+from collections import OrderedDict
+from timeit import timeit as ti
+
+
+class TestODict:
+    def __init__(self, maxsize):
+        self.obj = OrderedDict({x: x ** 2 + 1 for x in range(maxsize)})
+
+    def append(self, key, value):
+        self.obj[key] = value
+
+    def get(self, key):
+        return self.obj[key]
+
+    def pop(self, key):
+        return self.obj.pop(key, 0)
+
+    def popitem(self):
+        return self.obj.popitem()
+
+    def copy(self):
+        return self.obj.copy()
+
+    def clear(self):
+        self.obj.clear()
+
+
+class TestDict:
+    def __init__(self, maxsize):
+        self.obj = {x: x ** 2 + 1 for x in range(maxsize)}
+
+    def append(self, key, value):
+        self.obj[key] = value
+
+    def get(self, key):
+        return self.obj.pop(key, 0)
+
+    def pop(self, key):
+        return self.obj.pop(key, 0)
+
+    def popitem(self):
+        return self.obj.popitem()
+
+    def copy(self):
+        return self.obj.copy()
+
+    def clear(self):
+        self.obj.clear()
+
+
+objDict = TestDict(1000)
+objODict = TestODict(1000)
+
+print('Dict append {}'.format(ti(stmt="objDict.append('zzz', 'mmm')", globals=globals(), number=10000)))
+print('OrderedDict append {}'.format(ti(stmt="objODict.append('zzz', 'mmm')", globals=globals(), number=10000)))
+
+print('Dict get {}'.format(ti(stmt="objDict.get(88)", globals=globals(), number=10000)))
+print('OrderedDict get {}'.format(ti(stmt="objODict.get(88)", globals=globals(), number=10000)))
+
+# print('Dict popitem {}'.format(ti(stmt="testObj.popitem()", setup='testObj = TestDict(1000)', globals=globals(), number=10000)))
+# print('OrderedDict popitem {}'.format(ti(stmt="testObj.popitem()", setup='testObj = TestDict(1000)',globals=globals(), number=10000)))
+
+print('Dict pop {}'.format(ti(stmt="objDict.pop(88)", globals=globals(), number=10000)))
+print('OrderedDict pop {}'.format(ti(stmt="objODict.pop(88)", globals=globals(), number=10000)))
+
+print('Dict copy {}'.format(ti(stmt="objDict.copy()", globals=globals(), number=10000)))
+print('OrderedDict copy {}'.format(ti(stmt="objODict.copy()", globals=globals(), number=10000)))
+
+print('Dict clear {}'.format(ti(stmt="objDict.clear()", globals=globals(), number=10000)))
+print('OrderedDict clear {}'.format(ti(stmt="objODict.clear()", globals=globals(), number=10000)))
+
+"""
+Результаты
+Dict append 0.001920054999999997
+OrderedDict append 0.0020969659999999987
+Dict get 0.0027263650000000014
+OrderedDict get 0.001849227000000002
+Dict pop 0.002875133000000002
+OrderedDict pop 0.0035988640000000002
+Dict copy 0.071271807
+OrderedDict copy 0.906660559
+Dict clear 0.0019580959999998537
+OrderedDict clear 0.0020261399999998986
+
+Выводы
+Объекты практически идентичны по скорости работы
+Единственно небольшие различия при копировании объектов (функция copy)
+Использование OrderedDict обусловлено на уровне соглашения, что дает сторонным разработчиком понять что порядок
+добавления данных в словарь имеет значение для программы 
+"""
