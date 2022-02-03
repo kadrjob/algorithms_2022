96d44833bbb1372148418cd88c5ac07b3a8890ff
kadr <kadr_job@mail.ru>
2022-02-03 14:15:52 +0300

    Lesson6

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -13,3 +13,31 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+import time
+from memory_profiler import memory_usage
+from random import randint
+
+def decor(func):
+    def wrapper(*args, **kwargs):
+        time_start = time.perf_counter()
+        m1 = memory_usage()
+        res = func(args[0])
+        m2 = memory_usage()
+        mem_diff = m2[0] - m1[0]
+        time_end = time.perf_counter() - time_start
+        print(f"Выполнение {func.__name__} заняло {mem_diff} Mib. Время выполнения {time_end}")
+        return res
+
+    return wrapper
+
+@decor
+def test_func(m):
+    num = [randint(1, 50) for x in range(1, 2 * m + 2)]
+    i = 1
+    while i <= len(num) // 2:
+        num.remove(max(num))
+        i += 1
+    return max(num)
+
+print(test_func(10))
